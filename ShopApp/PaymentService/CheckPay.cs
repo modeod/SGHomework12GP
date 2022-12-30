a3b93c34c717909cc34642d980ecfce7ff76b4e2
Kasianenko Viacheslav <55177392+KasianenkoViacheslavMiki@users.noreply.github.com>
2022-12-31 01:25:09 +0200

    Stub Payment

---
+++ ShopApp/PaymentService/CheckPay.cs
@@ -0,0 +1,18 @@
+﻿using ShopApp.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.PaymentService
+{
+    public class CheckPay : IPayment
+    {
+        public bool Pay(uint _amount)
+        {
+            Console.WriteLine("Оплата готівкою");
+            return true;
+        }
+    }
+}
