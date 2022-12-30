9ef9c171dd19b7a63699f81cfd431c3d8d8630e3
Olexandr Sidletsky <scasha2000@gmail.com>
2022-12-30 22:46:26 +0200

    add DbContext

--- ShopApp/ShopDbContext.cs
+++ ShopApp/ShopDbContext.cs
@@ -13,11 +13,6 @@ namespace ShopApp
 {
     public class ShopDbContext : DbContext
     {
-        //public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options)
-        //{
-
-        //}
-
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlServer($"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ShopDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
 
