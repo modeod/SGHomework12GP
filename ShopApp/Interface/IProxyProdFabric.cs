6a4f08698d90fffb7debc8a24866a82997073303
EugeneDorosh <evdorow@gmail.com>
2023-01-02 02:26:29 +0200

    proxy interface

---
+++ ShopApp/Interface/IProxyProdFabric.cs
@@ -0,0 +1,15 @@
+﻿using GroupProject.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Interface
+{
+    public interface IProxyProdFabric : IProductFactory
+    {
+        public ProductDTO CreateProduct();
+        public void ChooseProductType();
+    }
+}
