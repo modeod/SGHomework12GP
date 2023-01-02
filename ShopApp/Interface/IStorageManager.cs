fd100b0a1c2507b0cb36a5a3a6900ad93c8db09e
Kasianenko Viacheslav <55177392+KasianenkoViacheslavMiki@users.noreply.github.com>
2023-01-02 17:20:04 +0200

    Init StorageManagerConsole

---
+++ ShopApp/Interface/IStorageManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Interface
+{
+    public interface IStorageManager
+    {
+        public void ShowMenu();
+        public void CreateProduct();
+        public void DeleteProduct();
+        public void ReadProduct();
+        public void UpdateProduct();
+    }
+}
---
+++ ShopApp/StorageManagerConsole.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using GroupProject.DTO;
+using Microsoft.EntityFrameworkCore;
+using ShopApp.Entities.ProductEntity;
+using ShopApp.Interface;
+using ShopApp.PaymentService;
+using ShopApp.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp
+{
+    public class StorageManagerConsole : IStorageManager
+    {
+        private ICRUDStorage storageCRUD;
+        private IProxyProdFabric prodFabric;
+
+        public StorageManagerConsole(ICRUDStorage storageCRUD, IProxyProdFabric prodFabric)
+        {
+            this.storageCRUD = storageCRUD;
+            this.prodFabric = prodFabric;
+        }
+        public void ShowMenu()
+        {
+            throw new NotImplementedException();
+        }
+ 
+        public void CreateProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadProduct()
+        {
+            throw new NotImplementedException();
+        }
+        public void UpdateProduct()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
