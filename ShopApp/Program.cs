8548998ffde573c7645b40f734ba2fb7c724aeb5
Stanislav <stas.budzyn@gmail.com>
2023-01-02 11:12:12 +0100

    added null check in repos

--- ShopApp/Program.cs
+++ ShopApp/Program.cs
@@ -1,18 +1,1 @@
-﻿using ShopApp;
-using ShopApp.Repositories;
-
-var repo = new StorageRepository(new ShopDbContext());
-var product = new ShopApp.Entities.ProductEntity.Product()
-{
-    VendorCode = 5,
-    ManufacterId = 2
-};
-try
-{
-    await repo.CreateProduct(null);
-}
-catch (Exception ex)
-{
-
-    Console.WriteLine(ex.Message);
-}
+﻿
