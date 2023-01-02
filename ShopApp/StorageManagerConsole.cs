3004cdc11fd8af8e19c098ab8564522ea9457875
Kasianenko Viacheslav <55177392+KasianenkoViacheslavMiki@users.noreply.github.com>
2023-01-02 17:27:23 +0200

    Implemented ShowMenu

--- ShopApp/StorageManagerConsole.cs
+++ ShopApp/StorageManagerConsole.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using GroupProject.DTO;
+﻿using GroupProject.DTO;
 using Microsoft.EntityFrameworkCore;
 using ShopApp.Entities.ProductEntity;
 using ShopApp.Interface;
@@ -26,7 +25,60 @@ namespace ShopApp
         }
         public void ShowMenu()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("1 - Добавити продукт");
+            Console.WriteLine("2 - Видалити продукт");
+            Console.WriteLine("3 - Оновити продукт");
+            Console.WriteLine("4 - Показати продукти");
+            Console.WriteLine("5 - Вихід");
+
+            int menuCount;
+            do
+            {
+                Console.WriteLine("Введіть ваш вибір: ");
+                if (!int.TryParse(Console.ReadLine(), out menuCount))
+                {
+                    Console.WriteLine("Потрібно ввести хоть якесь число!");
+                    continue;
+                }
+                else
+                if (menuCount < 1 || menuCount > 5)
+                {
+                    Console.WriteLine("Введіть дані числа меню!");
+                }
+            }
+            while (menuCount < 1 || menuCount > 5);
+
+            switch (menuCount)
+            {
+                case 1:
+                    {
+                        CreateProduct();
+                        break;
+                    }
+                case 2:
+                    {
+                        DeleteProduct();
+                        break;
+                    }
+                case 3:
+                    {
+                        ReadProduct();
+                        break;
+                    }
+                case 4:
+                    {
+                        UpdateProduct();
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("Вихід.");
+                        break;
+                    }
+                default:
+                    throw
+                        new Exception("Помилка меню.");
+            }
         }
  
         public void CreateProduct()
